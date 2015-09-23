8fb1992fd4cd193f40594f7c9487704f194cc66c
bseznec <blandine.seznec@insa-rennes.fr>
2015-09-23 12:08:22 +0200

    ajout map player et race au diagramme de classes

---
+++ ClassLibrary1/ClassLibrary1/Map.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Map
+    {
+        private int size;
+        private Player[] players;
+    }
+}
