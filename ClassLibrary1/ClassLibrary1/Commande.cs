74ae196014061a87c1aa912b6f6de003774ad625
laymard <laymrd@insa-rennes.fr>
2015-11-03 19:56:08 +0100

    ajout commande

---
+++ ClassLibrary1/ClassLibrary1/Commande.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public interface Command
+    {
+        /// <summary>
+        /// execute le mouvement
+        /// </summary>
+        void @do();
+        void redo();
+        void undo();
+        void canDo();
+        void done();
+    }
+}
---
+++ ClassLibrary1/ClassLibrary1/MoveUnits.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class MoveUnits : Command
+    {
+        /// <summary>
+        /// Exectue le mouvement et l'enregistre pour pouvoir le undo
+        /// </summary>
+        public void @do(int moveToX, int moveToY, Unit unit)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
