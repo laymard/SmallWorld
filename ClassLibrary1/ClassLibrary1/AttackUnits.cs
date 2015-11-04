be6160accc877f20d068868b1bd826066ef692ae
laymard <laymrd@insa-rennes.fr>
2015-11-04 23:32:31 +0100

    ajout attributs AttackUnits

--- ClassLibrary1/ClassLibrary1/AttackUnits.cs
+++ ClassLibrary1/ClassLibrary1/AttackUnits.cs
@@ -7,5 +7,61 @@ namespace ClassLibrary1
 {
     public class AttackUnits : Command
     {
+        public Coordinate lastCoord
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// nombre de dommages infligé
+        /// </summary>
+        public int damageGiven
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// nmbre de dommages subits
+        /// </summary>
+        public int damageTaken
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// unité blessé ou tué durant la dernière attaque
+        /// </summary>
+        public Unit ennemyAttacked
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
     }
 }
