b7b3bd9e59afe20d7e30ebac54287d3c24df59ef
laymard <laymrd@insa-rennes.fr>
2015-11-04 12:18:27 +0100

    ajout truc

--- ClassLibrary1/ClassLibrary1/MoveUnits.cs
+++ ClassLibrary1/ClassLibrary1/MoveUnits.cs
@@ -7,6 +7,63 @@ namespace ClassLibrary1
 {
     public class MoveUnits : Command
     {
+        public Unit Unit
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// point de mouvement nécessaire à ce mouvement
+        /// </summary>
+        public int cost
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// vecteur x de mouvement
+        /// </summary>
+        public int x
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// vecteur y de mouvement
+        /// </summary>
+        public int y
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
         /// <summary>
         /// Exectue le mouvement et l'enregistre pour pouvoir le undo
         /// </summary>
