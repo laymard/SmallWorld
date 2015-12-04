140ac89ac5b469e3ad4259642b8ef5ae2e81255d
laymard <laymard@insa-rennes.fr>
2015-12-04 22:31:27 +0100

    ajout attackUnits et MoveUnits

---
+++ ClassLibrary1/ClassLibrary1/Actors/MoveUnits.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class MoveUnits : Command
+    {
+        public Unit Unit
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// point de mouvement nécessaire à ce mouvement
+        /// </summary>
+        public int cost
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// position avant le mouvement
+        /// </summary>
+        public Coordinate lastCoord
+        {
+            get;
+            set;
+        }
+        void Command.execute()
+        {
+ 	        throw new NotImplementedException();
+        }
+
+        void Command.redo()
+        {
+ 	        throw new NotImplementedException();
+        }
+
+        void Command.undo()
+        {
+ 	        throw new NotImplementedException();
+        }
+
+        void Command.canDo()
+        {
+ 	        throw new NotImplementedException();
+        }
+
+        void Command.done()
+        {
+ 	        throw new NotImplementedException();
+        }
+    }
+}
---
+++ ClassLibrary1/ClassLibrary1/AttackUnits.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class AttackUnits : Command
+    {
+        public Coordinate lastCoord
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// nombre de dommages infligé
+        /// </summary>
+        public int damageGiven
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// nmbre de dommages subits
+        /// </summary>
+        public int damageTaken
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// unité blessé ou tué durant la dernière attaque
+        /// </summary>
+        public Unit ennemyAttacked
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        void Command.execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        void Command.redo()
+        {
+            throw new NotImplementedException();
+        }
+
+        void Command.undo()
+        {
+            throw new NotImplementedException();
+        }
+
+        void Command.canDo()
+        {
+            throw new NotImplementedException();
+        }
+
+        void Command.done()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
