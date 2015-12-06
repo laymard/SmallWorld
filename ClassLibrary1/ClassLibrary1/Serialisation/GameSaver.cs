c032cb7c388814459d4fb5bfbc7b6559e1cc3c01
bseznec <blandine.seznec@insa-rennes.fr>
2015-12-06 23:19:57 +0100

    ajout classes gamesaver et playertest

---
+++ ClassLibrary1/ClassLibrary1/Serialisation/GameSaver.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ClassLibrary1
+{
+    [Serializable()]
+    public class GameSaver
+    {
+        public GameSaver(){}
+
+        public GameSaver(Game game)
+        {
+            this.Game = game;
+            this.Matrix = new MatrixSaver(game.Map.matrix);
+        }
+
+        [XmlElement()]
+        public Game Game { get; set; }
+
+        [XmlElement()]
+        public MatrixSaver Matrix { get; set; }
+
+
+
+        public void SaveGame(string path)
+        {
+            XmlSerializer gameSerializer = new XmlSerializer(typeof(GameSaver));
+            StreamWriter writer = new StreamWriter(path);
+            gameSerializer.Serialize(writer, this);
+
+            writer.Close();
+        }
+
+        public static GameSaver ChargeGame(string chemin)
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(GameSaver));
+            StreamReader reader = new StreamReader(chemin);
+            GameSaver gs = (GameSaver)deserializer.Deserialize(reader);
+            reader.Close();
+
+            return gs;
+        }
+    }
+}
---
+++ ClassLibrary1/TestUnitairesCore/PlayerTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+
+namespace TestUnitairesCore
+{
+    
+    [TestClass]
+    public class PlayerTest
+    {
+        [TestMethod]
+        public void TestGetUnitsOnTile()
+        {
+            Player p1 = new Player(Race.Human, "Robert", 4);
+            Player p2 = new Player(Race.Elf, "Gaston", 4);
+            List<Player> players = new List<Player>{p1,p2};
+
+            Game game = new Game(new StandardMap(), players);
+
+            Player currentPlayer = game.CurrentPlayer;
+            game.move(0,0);
+
+            List<Unit> unitsOnTile = currentPlayer.getUnitsOnTile(game.Map.getCoord(0,0));
+
+            Assert.IsTrue(unitsOnTile.Contains(game.CurrentPlayer.CurrentUnit));
+        }
+    }
+}
