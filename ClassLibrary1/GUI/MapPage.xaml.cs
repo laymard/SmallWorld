d5631948aa254c792b0fd6496be8938e62e67981
bseznec <blandine.seznec@insa-rennes.fr>
2016-01-05 23:54:01 +0100

    truc

--- ClassLibrary1/GUI/MapPage.xaml.cs
+++ ClassLibrary1/GUI/MapPage.xaml.cs
@@ -31,12 +31,11 @@ namespace GUI
             this.Game = gb.Game;
 
 
-            this.nbTiles = Game.Map.MapSize.NbTiles;
+            this.NbTiles = Game.Map.MapSize.NbTiles;
 
             // calcul du côté de la case
-            this.sizeTile = mapGrid.Height / this.nbTiles;
-            this.creerGrille();
-            this.afficheGrille();
+            this.TileSize = mapGrid.Height / this.NbTiles;
+            this.displayGrid();
         }
 
 
@@ -46,66 +45,53 @@ namespace GUI
             set;
         }
 
-        public int nbTiles
+        public int NbTiles
         {
             get;
             set;
         }
 
-        public double sizeTile
+        public double TileSize
         {
             get;
             set;
         }
 
 
-        // constructeur de bouton correspondant au format de la grille (taille)
-        public Button formatBouton(TileType type)
+        // constructeur de bouton correspondant au format de la grille
+        public Button createButton(TileType type)
         {
             Button bouton = new Button();
             SolidColorBrush color = new SolidColorBrush();
-            Uri uri = new Uri("/images/test.png", UriKind.Relative);
+            Uri uri = new Uri(@"images\test.png", UriKind.RelativeOrAbsolute);
+            
             switch(type){
                 case TileType.MOUNTAIN :
                     color = new SolidColorBrush(Colors.Brown);
-                    
                     break;
+
                 case TileType.WATER :
                     color = new SolidColorBrush(Colors.Blue);
                     break;
+
                 case TileType.PLAIN :
                     color = new SolidColorBrush(Colors.GreenYellow);
                     break;
+
                 case TileType.FOREST :
                     color = new SolidColorBrush(Colors.Green);
                     break;
             }
-
+            
+            addImage(bouton,uri);
             bouton.SetValue(Button.BackgroundProperty, color);
-            addImage(bouton, uri); 
 
-            bouton.SetValue(Button.WidthProperty, sizeTile);
-            bouton.SetValue(Button.HeightProperty, sizeTile);
+            bouton.SetValue(Button.WidthProperty, this.TileSize);
+            bouton.SetValue(Button.HeightProperty, this.TileSize);
 
             return bouton;
         }
 
-
-
-        // création de la grid dans la page xaml
-        public void creerGrille()
-        {
-            // création de la grid
-            for (int j = 0; j < this.nbTiles; j++)
-            {
-                mapGrid.ColumnDefinitions.Add(new ColumnDefinition());
-            }
-            for (int i = 0; i < this.nbTiles; i++)
-            {
-                mapGrid.RowDefinitions.Add(new RowDefinition());
-            }
-
-        }
 
         // Enlève tous les éléments présents dans la grid de la page xaml
         public void nettoieGrille()
@@ -114,22 +100,22 @@ namespace GUI
         }
 
 
-        /** Affiche la grille avec le bon format et les icones correspondant au numéro de Page de la grille numGrille et retourne la liste des boutons créer
-         * Grid cadre : grid de la page xaml concernée par l'affichage
-         * **/
-        public void afficheGrille()
+        /** Affiche la grille **/
+        public void displayGrid()
         {
-
-            // ajout des boutons et de leurs icones dans la grille
-            for (int j = 0; j < this.nbTiles; j++)
+            for (int j = 0; j < this.NbTiles; j++)
             {
-                for (int i = 0; i < this.nbTiles; i++)
+                // Création de la grille
+                mapGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                mapGrid.RowDefinitions.Add(new RowDefinition());
+
+                for (int i = 0; i < this.NbTiles; i++)
                 {
                     // placement du bouton dans la grille 
-                    Button bouton = this.formatBouton(this.Game.Map.getTile(i,j));
-                    bouton.SetValue(Grid.ColumnProperty, j);
-                    bouton.SetValue(Grid.RowProperty, i);
-                    mapGrid.Children.Add(bouton);
+                    Button button = this.createButton(this.Game.Map.getTile(i,j));
+                    button.SetValue(Grid.ColumnProperty, j);
+                    button.SetValue(Grid.RowProperty, i);
+                    mapGrid.Children.Add(button);
                 }
                 
             }
@@ -138,8 +124,9 @@ namespace GUI
         
 
         /**A partir de l'uri, crée l'image adapté à la taille du Bouton**/
-        public void addImage(Button bouton, Uri uri)
+        public void addImage(Button button, Uri uri)
         {
+
             // creation de l'image 
             Image image = new Image();
             BitmapImage SourceBi = new BitmapImage();
@@ -147,14 +134,19 @@ namespace GUI
             image.Source = SourceBi;
 
             // empeche l'icone de depasser du contour du bouton
-            image.SetValue(Image.HeightProperty, this.sizeTile);
-            image.SetValue(Image.WidthProperty, this.sizeTile);
+            image.SetValue(Image.HeightProperty, this.TileSize);
+            image.SetValue(Image.WidthProperty, this.TileSize);
+            image.SetValue(Image.HorizontalAlignmentProperty, HorizontalAlignment.Center);
+            image.SetValue(Image.VerticalAlignmentProperty, VerticalAlignment.Center);
 
             // ajout de l'image au bouton
-            Grid grilleBouton = new Grid();
-            grilleBouton.Children.Add(image);
+            Grid box = new Grid();
+            box.RowDefinitions.Add(new RowDefinition());
+            image.SetValue(Grid.RowProperty, 0);
+            
+            box.Children.Add(image);
 
-            bouton.Content = grilleBouton;
+            button.Content = box;
         }
        
     }
